03d8c536597b92a7e8c9146c4842a229b38b6f56
TheDudeFromCI <thedudefromci@gmail.com>
2020-08-18 07:23:51 -0700

    Create index.js

---
+++ lookAt_Bot/index.js
@@ -0,0 +1,19 @@
+const mineflayer = require('mineflayer')
+
+const bot = mineflayer.createBot({
+  host: 'localhost',
+  port: 37269,
+  username: 'lookAt_Bot'
+})
+
+function lookAtNearestPlayer () {
+  const playerFilter = (entity) => entity.type === 'player'
+  const playerEntity = bot.nearestEntity(playerFilter)
+  
+  if (!playerEntity) return
+  
+  const pos = playerEntity.position.offset(0, playerEntity.height, 0)
+  bot.lookAt(pos)
+}
+
+bot.on('physicTick', lookAtNearestPlayer)
