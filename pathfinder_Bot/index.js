abb86c6ba31a87ace6143e52da05908100ecf44b
TheDudeFromCI <thedudefromci@gmail.com>
2020-08-19 03:56:28 -0700

    Pathfinding v2

--- pathfinder_Bot/index.js
+++ pathfinder_Bot/index.js
@@ -1,10 +1,11 @@
 const mineflayer = require('mineflayer')
 const { pathfinder, Movements, goals } = require('mineflayer-pathfinder')
 const GoalFollow = goals.GoalFollow
+const GoalBlock = goals.GoalBlock
 
 const bot = mineflayer.createBot({
     host: 'localhost',
-    port: 36573,
+    port: 40947,
     username: 'pathfinder_Bot'
 })
 
@@ -28,4 +29,27 @@ function followPlayer() {
     bot.pathfinder.setGoal(goal, true)
 }
 
-bot.once('spawn', followPlayer)
+function locateEmeraldBlock () {
+    const mcData = require('minecraft-data')(bot.version)
+    const movements = new Movements(bot, mcData)
+    movements.scafoldingBlocks = []
+    bot.pathfinder.setMovements(movements)
+
+    const emeraldBlock = bot.findBlock({
+        matching: mcData.blocksByName.emerald_block.id,
+        maxDistance: 32
+    })
+
+    if (!emeraldBlock) {
+        bot.chat("I can't see any emerald blocks!")
+        return
+    }
+
+    const x = emeraldBlock.position.x
+    const y = emeraldBlock.position.y + 1
+    const z = emeraldBlock.position.z
+    const goal = new GoalBlock(x, y, z)
+    bot.pathfinder.setGoal(goal)
+}
+
+bot.once('spawn', locateEmeraldBlock)
